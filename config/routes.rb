b2eb56f31f3f95b72975cf4b6c45aa6648775eea
fernandohn <fernandohn@hotmail.com.br>
2013-05-05 16:23:36 -0300

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Microblog::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   # routes to static pages
   root to: 'static_pages#home'
@@ -8,7 +13,6 @@ Microblog::Application.routes.draw do
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
 
-  match '/signup',  to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
